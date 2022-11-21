ab4265a93c7a7029e796a30f52d5c775b85a11ef
surfiex <lsj1529@gmail.com>
2022-11-21 21:13:27 +0900

    i30 인터페이스 값 수정

--- selfdrive/car/hyundai/interface_community.py
+++ selfdrive/car/hyundai/interface_community.py
@@ -9,8 +9,8 @@ def get_params(candidate, ret):
   if candidate in CAR.ELANTRA_GT_I30:
     ret.mass = 1275. + STD_CARGO_KG
     ret.wheelbase = 2.7
-    ret.steerRatio = 16.
-    tire_stiffness_factor = 0.7
+    ret.steerRatio = 15.4 #default 16.
+    tire_stiffness_factor = 0.385 #default 0.7
     ret.centerToFront = ret.wheelbase * 0.4
   elif candidate in [CAR.GRANDEUR_IG, CAR.GRANDEUR_IG_HEV]:
     ret.mass = 1570. + STD_CARGO_KG
